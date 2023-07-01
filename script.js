b90fa49fe9cc2a85e9139811da1392582c56b4dd
LamaPirat <g.skrefsrud@gmail.com>
2023-07-01 17:14:06 +0200

    lets

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-async function callHuberman() {
-  try {
-    const response = await fetch("https://apihosttest-1-j4314398.deta.app");
-    const data = await response.json();
-    console.log(data); // Process the data here
-  } catch (error) {
-    console.log("Error:", error);
-  }
+function callHuberman(messagesArray) {
+  fetch("http://localhost:80")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("API request failed with status: " + response.status);
+      }
+    })
+    .then((data) => {
+      // Process the returned data
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
+function getMessagesContent() {
+  var messages = document.getElementsByClassName("content");
+  var messagesArray = [];
+
+  for (var i = 0; i < messages.length; i++) {
+    messagesArray.push(messages[i].textContent.trim());
+  }
+
+  return messagesArray;
+}
+
+// Example usage
+var messagesContent = getMessagesContent();
+console.log(messagesContent);
+
 function sendMessage() {
-  callHuberman();
   var messageInput = document.getElementById("message-input");
   var message = messageInput.value.trim();
 
   if (message !== "") {
     var messageContainer = document.createElement("div");
-    messageContainer.className = "message user1"; // Change to 'user2' for the other user
+    messageContainer.className = "message user2"; // Change to 'user2' for the other user
 
     var senderSpan = document.createElement("span");
     senderSpan.className = "sender";
@@ -32,6 +54,27 @@ function sendMessage() {
 
     messageInput.value = "";
   }
+  messagesArray = getMessagesContent();
+  hubermanReply = callHuberman(message);
+  writeReply(hubermanReply);
+}
+
+function writeReply(hubermanReply) {
+  var messageContainer = document.createElement("div");
+  messageContainer.className = "message user1"; // Change to 'user2' for the other user
+
+  var senderSpan = document.createElement("span");
+  senderSpan.className = "sender";
+  senderSpan.innerText = "Huberman"; // Change to 'User 2' for the other user
+
+  var contentDiv = document.createElement("div");
+  contentDiv.className = "content";
+  contentDiv.innerText = hubermanReply;
+
+  messageContainer.appendChild(senderSpan);
+  messageContainer.appendChild(contentDiv);
+
+  document.querySelector(".messages").appendChild(messageContainer);
 }
 
 let input = document.getElementById("message-input");
